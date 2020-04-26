223596074b3452eb3cbd4b7dedc7575e461f30dc
brown_buddah <ahmrc1703@learning.icmanitoba.ca>
2020-04-26 00:12:12 -0500

    wrote the util calss

--- Utilities.java
+++ Utilities.java
@@ -1,18 +1,53 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+/**
+ * Class Name: Utilities
+ * Methods: loadFileAsString
+ * Description: random methods to used in came
+ * 
+ * author @Brown_Buddah
+ * version(4/26/2020)
+ */
 public class Utilities {
 
+    /**
+     * Name: ParseInt
+     * @param String
+     * @return string of whatever is in file
+     */
     public static String loadFileAsString(String path){
-        StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder(); //to attatch characters to a string
         try {
-            BufferedReader scan = new BufferedReader(new FileReader(path));
-            String line;
+            BufferedReader scan = new BufferedReader(new FileReader(path));  //scanning the text
+            String line;  //to store each line
             while((line = scan.readLine()) != null){
                 builder.append(line + "\n");
-            }
+
+                scan.close();
+            }//runs as long as there is a next line
+
         } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+            e.printStackTrace();  //prints errors
+
+        }//to catch errors
+
+        return builder.toString();// retuns the whole thing as a string
+    }
+
+    /**
+     * Name: ParseInt
+     * @param int
+     * @return string as a number from string
+     */
+    public static int parseInt(String number){
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();  //print error to screen
+            return 0;  //so method dosent crash
+
+        }//cathing anything that isnt a numebr
     }
 }
